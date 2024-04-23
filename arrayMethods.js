a19acd1a84b9602b1b70b5cc483825e4dc9c6e0f
Buongiorno <mnbuongiorno@gmail.com>
2024-04-22 22:03:06 -0300

    Fixed string methods, added array methods

---
+++ arrayMethods.js
@@ -0,0 +1,32 @@
+let names = ["Peter", "Marie", "George"]; 
+
+// transformative 
+// let result = names.pop(); deletes the last element of an array and return it
+// let result = names.shift(); deletes the first element of an array and return it
+// let result = names.push("Jhon", "Robert"); add strings into an array at the end
+// let result = names.reverse(); reverse the order
+// let result = names.unshift(1, 3); add  strings into an array at start
+// let result = names.sort(); sort in lexicographic order
+// let result = names.splice(1,3, "Alice"); (1 position to start to, 3 quantity to delete, "Alice" add number or string)
+
+
+
+// accessory
+// let result = names.join("-"); turn into string and add separate values with the ("-"), return new array
+// let result = names.slice(0,3); like substring but elements instead of chars.
+// toString(), indexOf(), lastIndexOf(), includes()
+
+//repetitive
+
+let result = names.filter((name)=>{
+    document.write(name +"<br>")
+}); // create new array with all the elements that meet the condition
+
+let result2 = names.filter(name => name.length > 6)
+
+// forEach() execute the indicated function one time for element of the array. 
+
+document.write(result2)
+
+document.write(result + "<br>")
+document.write(names)
--- stringMethods.js
+++ stringMethods.js
@@ -3,25 +3,25 @@ let numbers = 123;
 
 // let string2 = "String 2";
 
-// result = string.concat(string2); concat one or more strings and returns a new one
-// result = string.startsWith(string2); if one string starts with another string, return true, else return false
-// result = string.endsWith(string2); if one string ends with another string, return true, else return false
-// result = string.includes(string2); search strings into another strings, return boolean
-// result = string.indexOf("String"); search index and return index value, else return -1
-// result = string.lastIndexOf("String"); return last index char of the string, else return -1
+// let result = string.concat(string2); concat one or more strings and returns a new one
+// let result = string.startsWith(string2); if one string starts with another string, return true, else return false
+// let result = string.endsWith(string2); if one string ends with another string, return true, else return false
+// let result = string.includes(string2); search strings into another strings, return boolean
+// let result = string.indexOf("String"); search index and return index value, else return -1
+// let result = string.lastIndexOf("String"); return last index char of the string, else return -1
 
-// result = string.padStart(6,"T"); fills the string with chars at the start 
-// result = string.padEnd(3,"S"); fills the string with chars at end
-// result = string.repeat(2); return repeated string equal to number 
+// let result = string.padStart(6,"T"); fills the string with chars at the start 
+// let result = string.padEnd(3,"S"); fills the string with chars at end
+// let result = string.repeat(2); return repeated string equal to number 
 
-// result = string.split(" ") split the string and returns array
-// result = string.substring(0,3);  create new string (0 init, 3 fin)
-// result = string.toLowerCase(); 
-// result = string.toUpperCase();
-// result = numbers.toString();
+// let result = string.split(" ") split the string and returns array
+// let result = string.substring(0,3);  create new string (0 init, 3 fin)
+// let result = string.toLowerCase(); 
+// let result = string.toUpperCase();
+// let result = numbers.toString();
 
-// result = string.trim(); delete empty spaces
-// result = string.trimEnd(); delete empty spaces at end
-// result = string.trimStart(); delete empty spaces at start
+// let result = string.trim(); delete empty spaces
+// let result = string.trimEnd(); delete empty spaces at end
+// let result = string.trimStart(); delete empty spaces at start
 
 document.write(result);
