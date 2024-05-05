e7fb9502d64f32dd10f2751a286e8a0e83d09eb8
Buongiorno <mnbuongiorno@gmail.com>
2024-05-05 12:51:52 -0300

    Exercise 9 solved

--- exercise9.js
+++ exercise9.js
@@ -4,3 +4,43 @@
 //Organizar las actividades diaramente
 //Utilizar la consola para organizar todo.
 //El tiempo por tarea no debe superar las 4 hs.
+
+
+
+// 30  mins por dia para las tareas del hogar.
+// 100 mins tps
+// 10  mins descanso
+// 100 mins estudiar
+// 240 mins trabajo 
+
+
+let work = "Work 240 minutes";
+let study = "Study 100 minutes";
+let PW = "PW 100 minutes";
+let homeTasks = "Home tasks 30 minutes";
+let rest = "Rest 10 minutes"
+
+
+console.log('Tasks');
+
+for (let i = 0; i < 14; i++){
+    if (i == 0) {
+        console.groupCollapsed("Week 1")
+    }
+    console.group('Day '+ (i+1));
+    console.log(work);
+    console.log(rest);
+    console.log(study);
+    console.log(PW);
+    console.log(homeTasks);
+    console.groupEnd();
+    if (i == 7){
+            console.groupEnd();
+            console.groupCollapsed("Week 2")
+    }
+}
+
+console.groupEnd();
+console.groupEnd();
+
+
